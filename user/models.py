ea457497861190e7009874dca4b58d836db40698
Milan Biranwar <milanbiranwar06@gmail.com>
2022-10-31 12:56:14 +0530

    Adding auth user model program

--- user/models.py
+++ user/models.py
@@ -3,12 +3,6 @@ from django.db import models
 
 # Create your models here.
 
-
-class User(models.Model):
-    first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
-    username = models.CharField(max_length=200, unique=True)
-    password = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
+class User(AbstractUser):
     mob_number = models.IntegerField(default=10)
     location = models.CharField(max_length=200)
--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
 import json
 import logging
 
+from django.contrib.auth import authenticate
 # Create your views here.
 from django.http import JsonResponse
 
@@ -19,16 +20,17 @@ def user_registration(request):
     try:
         obj = json.loads(request.body)
         if request.method == 'POST':
-            user = User.objects.create(first_name=obj.get('first_name'), last_name=obj.get('last_name'),
-                                       username=obj.get('username'), password=obj.get('password'),
-                                       email=obj.get('email'), mob_number=obj.get('mob_number'),
-                                       location=obj.get('location'))
+            user = User.objects.create_user(first_name=obj.get('first_name'), last_name=obj.get('last_name'),
+                                            username=obj.get('username'), password=obj.get('password'),
+                                            email=obj.get('email'), mob_number=obj.get('mob_number'),
+                                            location=obj.get('location'))
             return JsonResponse({"Message": "User Registered",
                                  "data": {"id": user.id, "first_name": user.first_name, "last_name": user.last_name,
-                                          "username": user.username, "password": user.password, "email": user.email,
+                                          "username": user.username, "email": user.email,
                                           "mob_number": user.mob_number, "location": user.location}}, status=201)
         return JsonResponse({"Message": "Method not allowed"}, status=400)
     except Exception as e:
+        print(e)
         logging.error(e)
         return JsonResponse({"message": str(e), }, status=400)
 
@@ -40,10 +42,11 @@ def user_login(request):
     try:
         obj = json.loads(request.body)
         if request.method == 'POST':
-            user = User.objects.filter(username=obj.get('username'), password = obj.get('password')).first()
+            # user = User.objects.filter(user_name=obj.get('user_name'), password = obj.get('password')).first()
+            user = authenticate(username=obj.get('username'), password=obj.get('password'))
             if user is not None:
-                 return  JsonResponse({"Message": "login successful"})
-            return JsonResponse({"Message": "Invalid Credential"}, status=204)
+                return JsonResponse({"Message": "login successful"}, status=200)
+            return JsonResponse({"Message": "Invalid Credential"}, status=403)
         return JsonResponse({"Message": "Method not allowed"}, status=400)
 
     except Exception as e:
