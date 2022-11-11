59f0b8aeda44c8dd93df30c4b92c1ad9b8149073
Milan Biranwar <milanbiranwar06@gmail.com>
2022-11-11 06:09:40 +0530

    [Milan] Updating code for chat room

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,7 +1,5 @@
-# chat/consumers.py
 import json
 
-from asgiref.sync import sync_to_async
 from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
 
@@ -13,41 +11,30 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = "chat_%s" % self.room_name
 
-
-        # Join room group
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
 
     async def disconnect(self, close_code):
-        # Leave room group
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
-    # Receive message from WebSocket
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
         username = text_data_json['username']
         room_name = text_data_json['room_name']
 
-
-        # Send message to room group
         await self.save_messages(room_name, username, message)
         await self.channel_layer.group_send(
             self.room_group_name, {"type": "chat_message", "username": username, "message": message}
         )
 
-    # Receive message from room group
     async def chat_message(self, event):
         username = event["username"]
         message = event["message"]
 
-        # Send message to WebSocket
         await self.send(text_data=json.dumps({"username": username, "message": message}))
-
 
     @database_sync_to_async
     def save_messages(self, room_name, username, message):
         group = Group.objects.get(name=room_name, user__username=username)
         Messages.objects.create(group=group, user=group.user, message=message)
-
-
--- chat/room_view.py
+++ chat/room_view.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
@@ -6,11 +8,19 @@ from chat.models import Messages
 
 @login_required
 def index(request):
-    return render(request, "chat/index.html")
+    try:
+        return render(request, "chat/index.html")
+    except Exception as e:
+        logging.error(e)
+        return render(request, "chat/index.html")
 
 
 @login_required
 def room(request, room_name):
-    messages = Messages.objects.filter(group__name=room_name)
-    return render(request, "chat/room.html",
-                  {"username": request.user.username, "message_list": messages, "room_name": room_name})
+    try:
+        messages = Messages.objects.filter(group__name=room_name)
+        return render(request, "chat/room.html",
+                      {"username": request.user.username, "message_list": messages, "room_name": room_name})
+    except Exception as e:
+        logging.error(e)
+        return render(request, "chat/room.html")
